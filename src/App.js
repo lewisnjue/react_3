942ff7fbafe00b7e2ea9eefeb867314f66cc3546
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-07-16 13:00:29 +0300

    user reducer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 
 import './App.css';
 import React from 'react';
-import PropTypes from 'prop-types'
 
-const ChildComponent = (props) =>{
-  const styleinline = {
-    background:"green",
-    color:"yellow"
-  } // -> inline css 
-  return <h1 style={styleinline}>{props.title}</h1>
+import { useReducer } from 'react';
+
+
+const initialState = { count: 0 }; // -> initail state set to an object with count property 
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 };
 
-ChildComponent.propTypes = {
-  title: PropTypes.string.isRequired
-} ;// ->  this makes sure that the title is a string 
+const MyComponent = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+    </div>
+  );
+};
 
 
-ChildComponent.defaultProps = {
-  title: 'Default Title'
-}; // -> this will provide a default title 
-
-
-
-
-/* class ChildComponent extends React.Component{
-  render (){
-    return <h1>{this.props.title }</h1>
-  }
-} */
 function App() {
   return (
-    <ChildComponent title='lewis' />
+    < MyComponent />
 
   );
 }
